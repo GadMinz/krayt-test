e1325d9f73d3346932988fe15d4f0220319ba2a1
GadMinz <baykov97@mail.ru>
2023-01-30 15:25:58 +0300

    Add sale section

--- js/script.js
+++ js/script.js
@@ -1,25 +1,26 @@
 // Dropdown-menu
-const optionMenu = document.querySelector(".select-menu"),
-    selectBtn = optionMenu.querySelector(".select-btn"),
-    options = optionMenu.querySelectorAll(".option"),
-    selectBtnText = optionMenu.querySelector(".select-btn-text");
+const optionsMenus = document.querySelectorAll(".select-menu");
 
-let selectedOption = optionMenu.querySelector(".option.selected");
-selectBtnText.innerText = selectedOption.innerText;
-
-selectBtn.addEventListener("click", () => {
-    optionMenu.classList.toggle("open");
-});
-
-options.forEach((option) => {
-    option.addEventListener("click", () => {
-        selectedOption.classList.remove("selected");
-        option.classList.add("selected");
-        selectedOption = option;
-        selectBtnText.innerText = selectedOption.innerText;
-        optionMenu.classList.remove("open");
+optionsMenus.forEach((optionMenu) => {
+    const selectBtn = optionMenu.querySelector(".select-btn"),
+        options = optionMenu.querySelectorAll(".option"),
+        selectBtnText = optionMenu.querySelector(".select-btn-text");
+    let selectedOption = optionMenu.querySelector(".option.selected");
+    selectBtnText.innerText = selectedOption.innerHTML;
+    selectBtn.addEventListener("click", () => {
+        optionMenu.classList.toggle("open");
+    });
+    options.forEach((option) => {
+        option.addEventListener("click", () => {
+            selectedOption.classList.remove("selected");
+            option.classList.add("selected");
+            selectedOption = option;
+            selectBtnText.innerText = selectedOption.innerText;
+            optionMenu.classList.remove("open");
+        });
     });
 });
+
 // Nav-menu
 const iconNav = document.querySelector(".nav-icon");
 const nav = document.querySelector(".nav");
@@ -49,3 +50,36 @@ const createSlider = (id, slidesPerView, loop = true, breakpoints = {}) => {
 };
 
 createSlider(".super-discount-swiper", 1);
+createSlider(".sale-swiper", 4, false, {
+    0: {
+        slidesPerView: 1,
+    },
+    620: {
+        slidesPerView: 2,
+    },
+    930: {
+        slidesPerView: 3,
+    },
+    1240: {
+        slidesPerView: 4,
+    },
+});
+createSlider(".sale-card-img-slider", 1);
+createSlider(".day-product-card-img-swiper", 1, false);
+
+// Sale Cards
+const cards = document.querySelectorAll(".sale-card-wrapper");
+document.onclick = (e) => {
+    cards.forEach((card) => {
+        card.classList.remove("active");
+    });
+};
+cards.forEach((card) => {
+    card.onclick = (e) => {
+        cards.forEach((card) => {
+            card.classList.remove("active");
+        });
+        card.classList.add("active");
+        e.stopPropagation();
+    };
+});
