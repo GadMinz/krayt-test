3f53cc045e3491697208735e6f79d6d52a004e4e
GadMinz <baykov97@mail.ru>
2023-01-30 17:06:07 +0300

    Add day-product section

--- js/script.js
+++ js/script.js
@@ -32,6 +32,7 @@ iconNav.addEventListener("click", () => {
 });
 
 //Sliders
+
 const createSlider = (id, slidesPerView, loop = true, breakpoints = {}) => {
     const swiper = new Swiper(id, {
         loop,
@@ -65,6 +66,7 @@ createSlider(".sale-swiper", 4, false, {
     },
 });
 createSlider(".sale-card-img-slider", 1);
+createSlider(".day-product-swiper", 1, false);
 createSlider(".day-product-card-img-swiper", 1, false);
 
 // Sale Cards
@@ -82,4 +84,26 @@ cards.forEach((card) => {
         card.classList.add("active");
         e.stopPropagation();
     };
+});
+
+//Timer
+
+let date = new Date().setDate(new Date().getDate() + 1); //expiration date
+const timers = document.querySelectorAll(".timer");
+window.scrollX;
+timers.forEach((timer) => {
+    const h = timer.querySelector(".h"),
+        m = timer.querySelector(".m"),
+        s = timer.querySelector(".s");
+    const counts = () => {
+        let now = new Date();
+        let gap = date - now;
+        let hours = Math.floor(gap / 1000 / 60 / 60);
+        let minutes = Math.floor((gap / 1000 / 60) % 60);
+        let seconds = Math.floor(gap / 1000) % 60;
+        h.innerText = hours;
+        m.innerText = minutes;
+        s.innerText = seconds;
+    };
+    setInterval(counts, 1000);
 });
